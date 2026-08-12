08fdbdd132786b0adc718876a6304c9f96fb5ca9
Moshe Lev Ceder <navkc11@gmail.com>
2026-08-12 13:36:48 +0300

    add project skeleton with main components and context setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { AppProvider } from "./context/AppContext";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div>autopia</div>;
+  return (
+    <AppProvider>
+      <div className="app-container" dir="rtl">
+        <Header />
+        <Navbar />
+        <main>
+          <HomePage />
+        </main>
+      </div>
+    </AppProvider>
+  );
 }
 
 export default App;
---
+++ src/components/VehicleWizard.jsx
@@ -0,0 +1,3 @@
+export default function VehicleWizard() {
+  return <div>VehicleWizard</div>;
+}
