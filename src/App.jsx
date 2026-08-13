db20cb5d10d355109ca61dd9e2fcf933470b3012
Daniel Chernov <chernov.dani@gmail.com>
2026-08-13 10:13:55 +0300

    refactor: remove AppProvider context and simplify App component structure feat: add .gitkeep files to services, stores, and utils directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import { AppProvider } from "./context/AppContext";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <AppProvider>
-      <div className="app-container" dir="rtl">
-        <Header />
-        <Navbar />
-        <main>
-          <HomePage />
-        </main>
-      </div>
-    </AppProvider>
+    <div className="app-container" dir="rtl">
+      <Header />
+      <Navbar />
+      <main>
+        <HomePage />
+      </main>
+    </div>
   );
 }
 
